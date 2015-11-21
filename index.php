e44e01c62a0666389f06d54e0f7984f27981a53c
Programming <Programming@lab08.flpl.local>
2015-11-21 11:18:24 -0600

    HEADERS

--- index.php
+++ index.php
@@ -10,12 +10,22 @@
 		<table>
 			<?php
 				$connection = mysqli_connect("localhost","piggybank","hitlerlovesnutella1","piggybank");
+				$query = "select column_name from information_SCHEMA.columns where table_name='currency' and table_schema='piggybank' ";
 				$query = "select * from piggybank.currency";
 
 				$results = mysqli_query($connection, $query);
-				if(!$results){
+				$headers = mysqli_query($connection, $query);
+
+				if(!$results || !$headers){
 					die("This is an errror message. Have a nice day ".mysqli_error() );
 				}
+
+				echo "<tr>";
+				while($header = mysql_fetch_array($headers, MYSQLI_NUM)){
+					echo "<th>".$header[0]."</th>";
+				}
+				echo "</tr>";
+
 
 				while($row = mysqli_fetch_array($results)){
 					echo "<tr>";
@@ -26,8 +36,12 @@
 					echo "<td>".$row["country"]."</td>";
 					echo "<td>".$row["year"]."</td>";
 					echo "</tr>";
-
 				}
+
+
+				mysqli_close($connection);
+
+
 			?>
 		</table>
 	</body>
