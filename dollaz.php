1884549d920d037dd992bfd580ef09947850987b
Programming <Programming@lab08.flpl.local>
2015-11-21 12:11:04 -0600

    Implemented insert query

--- dollaz.php
+++ dollaz.php
@@ -1,4 +1,12 @@
 <?php 
-$query="insert into piggybank (name, type, denomination, country, year) '".$_POST['name']."','".$_POST['type']."',".$_POST['denomination'].",'".$_POST['country']."',".$_POST['year'];
-echo $query
+$query="insert into piggybank (name, type, denomination, country, year) values('".$_POST['name']."','".$_POST['type']."',".$_POST['denomination'].",'".$_POST['country']."',".$_POST['year'].")";
+$connection = mysqli_connect("localhost","piggybank","hitlerlovesnutella1","piggybank");
+
+$result=mysqli_query($connection, $query);
+if(!$result){
+	echo $query;
+	die(mysqli_error($connection));
+}
+
+header("Location: index.php");
  ?>
